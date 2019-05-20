2d99a1b0e067a71733d7c1ae1edaed14cc312e68
NickWolfen99 <strftwolfen99@gmail.com>
2019-05-20 21:56:05 +0300

    as

--- twonumbersx/Program.cs
+++ twonumbersx/Program.cs
@@ -10,6 +10,38 @@ namespace twonumbersx
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter number of elements in S:");
+            int n = Convert.ToInt16(Console.ReadLine());
+            int[] S = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"S[{i + 1}]=");
+                S[i] = Convert.ToInt32(Console.ReadLine());
+
+
+            }
+            Console.Write("Enter the value of X:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(TwoIntegersSumToTarget(S,x));
+            Console.ReadLine();
         }
+        
+          public static bool TwoIntegersSumToTarget(int[] S, int x)
+          {
+               for (int i = 0; i < S.Length; i++)
+               {
+                   for (int j = i+1; j < S.Length; j++)
+                   {
+  
+                         if (S[i] + S[j] == x)
+                         return true;
+                      
+                   }
+               }
+              return false;
+          }
+        
     }
 }
